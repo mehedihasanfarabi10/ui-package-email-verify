d5e23cc414ead45df24baa00195bf6e28fb10da4
Mehedi Hasan Farabi <mehedihasanfarabi10@gmail.com>
2024-10-13 22:25:36 +0600

    Cache driver file and database

--- app/Http/Controllers/FrontendController.php
+++ app/Http/Controllers/FrontendController.php
@@ -4,13 +4,35 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class FrontendController extends Controller
 {
     //
     public function index(){
+        // $posts = "My love ";
+        // Cache::add('post',$posts,now()->addDay());
+        // Cache::put('post',$posts,now()->addDay());
+        // Cache::forget('post'); // remove
+        //Cache::forever('post'); //never delete
+        // Cache::flush(); //all delete
+        // $post = Post::all();
+        // Cache::add('post',$post,now()->addDay());
+        // Cache::get('post');
 
-        $post = Post::all();
+    //    dd( Cache::get('post'));
+
+        //DB cache
+
+        $post = Cache::remember('mypost',40,function(){ //40 seconds
+            return Post::all();
+        });
+
+
+
+        //DB Cache end
+
+
         return view("welcome",compact('post'));
     }
 }
